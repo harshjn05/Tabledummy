bf09a61fcc92df14c63d95ebe8d60d28f2407219
harshjn05 <harsh.diwakirtis@gmail.com>
2022-06-13 13:37:35 +0530

    first commit

---
+++ src/column.js
@@ -0,0 +1,71 @@
+
+export const COLUMNS = [
+    {
+      Header: 'Id',
+      Footer: 'Id',
+      accessor: 'id',
+    },
+    {
+      
+      Header: 'First Name',
+      Footer: 'First Name',
+      accessor: 'first_name',
+  
+      width:200
+    },
+    {
+      Header: 'Last Name',
+      Footer: 'Last Name',
+      accessor: 'last_name',
+    },
+  
+    {
+      Header: 'Phone.........',
+      Footer: 'Phone',
+      accessor: 'contact no'
+    },
+    /*  {
+        Header: 'Email',
+        Footer: 'Email',
+        accessor: 'email'
+      },*/
+    {
+      Header: 'Gender',
+      Footer: 'Gender',
+      accessor: 'gender'
+    },
+    {
+      Header: 'IP Address',
+      Footer: 'IP Address',
+      accessor: 'ip_address'
+    },
+    {
+      Header: 'Initial_Balance',
+      Footer: 'Initial_Balance',
+      accessor: 'Initial balance'
+    },
+    {
+      Header: 'Final_Balance',
+      Footer: 'Final_Balance',
+      accessor: 'Final Balance'
+    },
+    {
+      Header: 'Company',
+      Footer: 'Company',
+      accessor: 'Company name'
+    },
+    {
+      Header: 'DEPT',
+      Footer: 'DEPT',
+      accessor: 'Department',
+    },
+    {
+      Header: 'Click here',
+      Footer: 'Click here',
+      accessor: 'email',
+  
+    },
+  
+  ]
+  
+  
